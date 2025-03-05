2ef7aa4ab20d68d1b605147ce75f64f4772e1bb4
ReyperAutomatizacion02 <reyper.automatizacion.2@gmail.com>
2025-03-05 15:20:36 -0600

    cambios para arrancar render

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template, jsonify
 from mapDB import extraer_relaciones  # Tu función para obtener datos de Notion
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 @app.route("/")
 def index():
     return render_template("index.html")  # Renderiza el HTML
 
-@app.route("/api/datos")
-def datos():
-    datos = extraer_relaciones()  # Llama a tu función para obtener datos de Notion
-    return jsonify(datos)  # Devuelve los datos en formato JSON
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render asigna el puerto automáticamente
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run()
